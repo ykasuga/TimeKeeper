dbe1620e73e226afc94133a8e592af77ad12bf2e
ykasuga <kasuga.yohei@gmail.com>
2021-02-11 01:40:19 +0900

    [add]TicketList class

---
+++ ticket_list.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@file ticket_list.py
+@author Y. Kasuga
+@date 2021/2/10
+@brief List of tickets.
+"""
+
+class TicketListOption(object):
+    """List of ticket
+    """
+    def __init__(self) -> None:
+        """Constructor of TicketListOption class.
+        """
+        super().__init__()
